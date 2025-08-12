b71ce3b2184bfaaee936ba753251b868373c1729
Robert Sofianu <robertsofianu@gmail.com>
2025-08-13 02:02:33 +0300

    ocr updates

--- api_svc/services/ocr_svc/ocr_svc.py
+++ api_svc/services/ocr_svc/ocr_svc.py
@@ -1,4 +1,11 @@
+import argparse
 from framework.ocr.ocr_api import OcrApi
 
-open_api = OcrApi(image_path="/Users/sofianurobert/Downloads/WhatsApp Image 2025-08-12 at 22.19.08.jpeg")
+parser = argparse.ArgumentParser(description="OCR Invoice Parser")
+parser.add_argument('--image_path', type=str, 
+                    default="/Users/sofianurobert/Downloads/WhatsApp Image 2025-08-12 at 22.19.08.jpeg",
+                    help='Path to the invoice image')
+args = parser.parse_args()
+
+open_api = OcrApi(image_path=args.image_path)
 print(open_api.parse_invoice())
